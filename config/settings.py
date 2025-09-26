6654cd32b173d3e5b7689c0900982348055fd4ac
alice <eeyeey999@gmail.com>
2025-09-26 15:59:25 +0800

    step 7 add debug toolbar

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,9 @@ DJANGO_APP = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',]
+    'django.contrib.staticfiles',
+    "debug_toolbar",
+    ]
 
 APPLICATION_APP = [
     'pages.apps.PagesConfig',
@@ -50,6 +52,7 @@ APPLICATION_APP = [
 INSTALLED_APPS = DJANGO_APP + APPLICATION_APP
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +135,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'config/staticfiles')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [ "127.0.0.1",]
