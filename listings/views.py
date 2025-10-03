be0b4c4409700ffdf5a27ab9e6f8a6c94db06192
alice <eeyeey999@gmail.com>
2025-10-03 14:43:31 +0800

    step 11 index template

--- listings/views.py
+++ listings/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Listing
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
-
-# def index(request):
-#    return HttpResponse("<h1>Hello World!</h1>")
-
 def listings(request):
-    listings=Listing.objects.all()
+    listings = Listing.objects.order_by('list_date').filter(is_published=True)
+    paginator=Paginator(listings,6)
+    page=request.GET.get('page')
+    paged_listings=paginator.get_page(page)
     # listings=Listing.objects.all().filter(is_published=True).order_by('-list_date')
-    context={'listings': listings}
+    context = {"listings": paged_listings}
     return render(request, 'listings/listings.html', context)
     # return render(request, 'listings/listings.html', {'name': 'Medical Center Listings'})  
 
